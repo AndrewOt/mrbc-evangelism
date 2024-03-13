d438855de504ae79886df68c9349d1d5d13f0e5f
Andrew <aottavianospam@outlook.com>
2024-03-13 18:17:20 -0500

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: "/mrbc-evangelism/",
   build: {
     outDir: "./_site",
   },
