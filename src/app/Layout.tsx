a2a8929d73b9788c1b3706e7b3c738eae67ed882
Andrew <aottavianospam@outlook.com>
2024-03-24 08:30:15 -0500

    Added auth

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,7 +1,9 @@
 import { AppBar, Typography, Box, Toolbar } from "@mui/material";
 import { Metadata } from "next";
 import BackButton from "../components/BackButton";
+import { ClerkProvider } from "@clerk/nextjs";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const metadata: Metadata = {
   title: "MRBC Evangelism",
 };
@@ -12,16 +14,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <AppBar>
-          <Toolbar>
-            <BackButton />
-            <Typography>MRBC Evangelism</Typography>
-          </Toolbar>
-        </AppBar>
-        <Box mt={8}>{children}</Box>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <AppBar>
+            <Toolbar>
+              <BackButton />
+              <Typography>MRBC Evangelism</Typography>
+            </Toolbar>
+          </AppBar>
+          <Box mt={8}>{children}</Box>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,4 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({});
+export const config = { matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/"] };
