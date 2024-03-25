01eddae8e4cc426b3ae4b1e82f4e5af9eaab55d7
Andrew <aottavianospam@outlook.com>
2024-03-25 12:35:40 -0500

    Config change. Hope this fixes vercel build.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: './dist', // Changes the build output directory to `./dist/`.
   // eslint-disable-next-line no-undef
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 }
